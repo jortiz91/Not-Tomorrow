70cda5729bd1c7204ce4354b867b5b6430810e79
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-07-14 16:12:48 -0400

    create routes for CRUD

--- routes/nutrition.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const nutritionCtrl = require('../controllers/nutrition')
-
-router.post('/exercises/:id/nutrition', nutritionCtrl.create)
-
-module.exports = router
---
+++ routes/workouts.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const router = express.Router()
+
+//Get all workouts
+router.get('/', (req, res) => {
+  res.json({ mssg: 'GET all workout' })
+})
+//Get one workout
+router.get('/:id', (req, res) => {
+  res.json({ mssg: 'GET one workout' })
+})
+//Post one workout
+router.post('/', (req, res) => {
+  res.json({ mssg: 'POST one workout' })
+})
+//Delete one workout
+router.delete('/:id', (req, res) => {
+  res.json({ mssg: 'DELETE one workout' })
+})
+//update
+router.put('/:id', (req, res) => {
+  res.json({ mssg: 'UPDATE one workout' })
+})
+
+module.exports = router
