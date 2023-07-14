db066fb7fe0db2058f273f9c443c3f21d7074fad
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-07-14 15:18:11 -0400

    update server.js backend

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
+require('dotenv').config()
 const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-
-// const AuthRouter = require('./routes/AuthRouter')
-// const PostRouter = require('./routes/PostRouter')
-
-const PORT = process.env.PORT || 3001
-
-const db = require('./db')
 
 const app = express()
 
-app.use(cors())
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-// app.use('/auth', AuthRouter)
-// app.use('/posts', PostRouter)
-
-app.use('/', (req, res) => {
-  res.send(`Connected!`)
+app.get('/', (req, res) => {
+  res.json({ mssg: 'Welcome to the app!' })
 })
 
-app.listen(PORT, () => {
-  console.log(`Running Express server on Port ${PORT} . . .`)
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
 })
