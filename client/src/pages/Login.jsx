ac7d938925f8b82ba720348dc6cac91776d528bf
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-07-18 11:51:11 -0400

    create functionality to have the logout button removed when logged in

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { login, error, isLoading } = useLogin()
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    console.log(email, password)
+    await login(email, password)
   }
 
   return (
@@ -26,7 +28,8 @@ const Login = () => {
         onChange={(event) => setPassword(event.target.value)}
         value={password}
       />
-      <button>Login</button>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   )
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,20 @@ const createToken = (_id) => {
 }
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: 'login user' })
+  const { email, password } = req.body
+
+  try {
+    const user = await (email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
+
 const signupUser = async (req, res) => {
   const { email, password } = req.body
   try {
