88742d1d17ad0c6ea2f8600c3bcd66f4329b8432
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-07-18 17:45:58 -0400

    create 2 additional mods

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 
 //get all workouts
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 })
+  const user_id = req.user._id
+
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 })
   res.status(200).json(workouts)
 }
 //get a single workout
@@ -23,9 +25,26 @@ const getWorkout = async (req, res) => {
 //create a workout
 const createWorkout = async (req, res) => {
   const { title, weight, reps } = req.body
+  let emptyFields = []
+
+  if (!title) {
+    emptyFields.push('title')
+  }
+  if (!weight) {
+    emptyFields.push('Weight')
+  }
+  if (!reps) {
+    emptyFields.push('reps')
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all the fields', emptyFields })
+  }
 
   try {
-    const workout = await Workout.create({ title, weight, reps })
+    const user_id = req.user._id
+    const workout = await Workout.create({ title, weight, reps, user_id })
     res.status(200).json(workout)
   } catch (e) {
     res.status(404).json({ e: e.message })
